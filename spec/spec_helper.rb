ba414f18fec4c1828f30374311b50cb325d70af5
Gabriel de Espindula <gabrieldeespindula80@gmail.com>
2025-08-29 19:10:47 -0300

    Setup specs configuration (#1)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -91,4 +91,8 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+
+  config.profile_examples = 10
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
 end
