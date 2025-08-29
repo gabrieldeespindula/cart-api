a56e78f1bf51a8c203edf77732c0d4572cbd5dc1
gabrieldeespindula <gabrieldeespindula80@gmail.com>
2025-08-29 20:14:03 -0300

    Fix cart routing specs

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
   resources :products
+  resource :cart, only: [:show, :create] do
+    post :add_item
+    delete ':product_id', to: 'carts#remove_item', as: :remove_item
+  end
+
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "rails/health#show"
